1be8f78a3617a848bc146588b718176bec564835
Даниил Бабушкин <nyta_210897@mail.ru>
2022-12-05 23:42:15 +0500

    Задание 5

--- IS-1-20-BabushkinDK-U/Task5.cs
+++ IS-1-20-BabushkinDK-U/Task5.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConnectDB;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static ConnectDB.Class1;
 
 namespace IS_1_20_BabushkinDK_U
 {
     public partial class Task5 : Form
     {
+        MySqlConnection conn;
+        Class1 connect;
         public Task5()
         {
             InitializeComponent();
+        }
+
+        private void Task5_Load(object sender, EventArgs e)
+        {
+            connect = new Class1();
+            connect.Connectreturn();
+            conn = new MySqlConnection(connect.connStr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fioStud = textBox1.Text;
+                string datetimeStud = textBox2.Text;
+                conn.Open();
+                string sql = $"INSERT INTO t_Uchebka_Babsuhkin(fioStud, datetimeStud) " +
+                    $"VALUES ('{fioStud}', '{String.Format("{0:s}", datetimeStud)}');" +
+                    $"SELECT idStud FROM t_Uchebka_Babsuhkin WHERE(idStud = LAST_INSERT_ID());";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteScalar();
+                MessageBox.Show("введены данные");               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
