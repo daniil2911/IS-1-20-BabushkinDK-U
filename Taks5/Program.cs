1b0702f54024fd77c844537c21cd0934642fd406
Даниил Бабушкин <nyta_210897@mail.ru>
2022-12-05 22:55:18 +0500

    1

--- Taks5/Program.cs
+++ Taks5/Program.cs
@@ -16,7 +16,7 @@ namespace Taks5
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form());
         }
     }
 }
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -16,7 +16,7 @@ namespace Task3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form());
         }
     }
 }
