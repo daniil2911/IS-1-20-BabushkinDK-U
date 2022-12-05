51cb0e6d8c15c6f44980503b3cbf5803302a4e94
Даниил Бабушкин <nyta_210897@mail.ru>
2022-12-05 22:56:59 +0500

    3 Задание

--- Task3/Task3.Designer.cs
+++ Task3/Task3.Designer.cs
@@ -67,7 +67,7 @@ namespace IS_1_20_BabushkinDK_U
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(775, 452);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
