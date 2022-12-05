11a437b72da4a4604730c2fa92b84a013bc5e920
Даниил Бабушкин <nyta_210897@mail.ru>
2022-12-05 22:57:29 +0500

    5 Задание

--- Taks5/Task5.Designer.cs
+++ Taks5/Task5.Designer.cs
@@ -29,10 +29,17 @@ namespace IS_1_20_BabushkinDK_U
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Task5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(814, 461);
+            this.Name = "Task5";
             this.Text = "Task5";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
